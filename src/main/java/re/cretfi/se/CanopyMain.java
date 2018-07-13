4e1e964c8ff0d78e1587050368b3ab05329846a2
Jeff Wang <jeffawang@gmail.com>
2018-07-13 03:54:39 -0700

    hello world server + servlet

--- src/main/java/re/cretfi/se/CanopyMain.java
+++ src/main/java/re/cretfi/se/CanopyMain.java
@@ -1,7 +1,14 @@
 package re.cretfi.se;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+
 public class CanopyMain {
-    public static void main(String[] args) {
-        System.out.println("hi");
+    public static void main(String[] args) throws Exception{
+        System.out.println("Starting server.");
+        Server server = new Server(7070);
+        ServletContextHandler handler = new ServletContextHandler(server, "/");
+        handler.addServlet(CanopyServlet.class, "/");
+        server.start();
     }
 }

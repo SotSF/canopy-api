032cdf440129ab06622a69b60f47f8037f2e9916
Jeff Wang <jeffawang@gmail.com>
2018-07-13 07:16:42 -0700

    Registry stats endpoint at /stats

--- src/main/java/re/cretfi/se/CanopyServlet.java
+++ src/main/java/re/cretfi/se/CanopyServlet.java
@@ -1,8 +1,10 @@
 package re.cretfi.se;
 
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.heroicrobot.dropbit.devices.pixelpusher.Pixel;
+import com.heroicrobot.dropbit.devices.pixelpusher.PixelPusher;
 import com.heroicrobot.dropbit.devices.pixelpusher.Strip;
 import com.heroicrobot.dropbit.registry.DeviceRegistry;
 import javafx.beans.Observable;
@@ -13,10 +15,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Observer;
+import java.util.*;
 
 public class CanopyServlet extends HttpServlet implements Observer {
 
@@ -86,6 +85,29 @@ public class CanopyServlet extends HttpServlet implements Observer {
         JsonObject stats = new JsonObject();
         stats.addProperty("pushing", this.pushing);
         stats.addProperty("hello", "world");
+        stats.addProperty("frameLimit", registry.getFrameLimit());
+        stats.addProperty("logEnabled", registry.getLogging());
+        stats.addProperty("totalPower", registry.getTotalPower());
+        stats.addProperty("totalPowerLimit", registry.getTotalPowerLimit());
+        stats.addProperty("powerScale", registry.getPowerScale());
+
+//        JsonArray pushers = new JsonArray();
+//
+//        // This blows up at runtime with NullPointerException for some reason.
+//        for (PixelPusher p : registry.getPusherMap().values()) {
+//            JsonObject pusher = new JsonObject();
+//            pusher.addProperty("numberOfStrips", p.getNumberOfStrips());
+//            pusher.addProperty("maxStripsPerPacket", p.getMaxStripsPerPacket());
+//            pusher.addProperty("pixelsPerStrip", p.getPixelsPerStrip());
+//            pusher.addProperty("pusherFlags", p.getPusherFlags());
+//            pusher.addProperty("updatePeriod", p.getUpdatePeriod());
+//            pusher.addProperty("powerTotal", p.getPowerTotal());
+//            pusher.addProperty("deltaSequence", p.getDeltaSequence());
+//            pusher.addProperty("extraDelay", p.getExtraDelay());
+//            pushers.add(pusher);
+//        }
+//
+//        stats.add("pushers", pushers);
 
         resp.getWriter().println(stats.toString());
         resp.setStatus(HttpStatus.OK_200);
